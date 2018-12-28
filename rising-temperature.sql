0103be07ffbf7fd669b3f405b2efa3069fe365c2
Chander Inguva <cinguva@users.noreply.github.com>
2018-12-28 15:49:43 -0600

    Create rising-temperature.sql
    https://leetcode.com/problems/rising-temperature/

---
+++ rising-temperature.sql
@@ -0,0 +1,9 @@
+# 197
+SELECT
+    weather.id AS 'Id'
+FROM
+    weather
+        JOIN
+    weather w ON DATEDIFF(weather.RecordDate, w.RecordDate) = 1
+        AND weather.Temperature > w.Temperature
+;
